ab17875c1f2243191a1b1f6ba63387f6db03c55e
ernaneaugusto <ernaneatcipriano@gmail.com>
2017-07-12 12:21:38 -0300

    arquivos minificados com gulp(img, css, js)

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+uglify = require('gulp-uglify'),
+clean = require('clean-css'),
+cssmin = require('gulp-cssmin'),
+rename = require('gulp-rename');
+
+gulp.task('build-img', function(){
+	gulp.src(['app/images/**/*', '!app/images/**/Thumbs.db'])
+	.pipe(imagemin())
+	.pipe(gulp.dest('app/images'));	
+});
+
+gulp.task('uglify-js', function () {
+    gulp.src(['app/js/**/*', '!app/js/**/Thumbs.db'])
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))    
+    .pipe(gulp.dest('app/js'));
+})
+
+gulp.task('clean-css', function () {
+    gulp.src(['app/css/**/*', '!app/css/**/Thumbs.db'])
+    .pipe(clean())
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('cssmin', function () {
+    gulp.src(['app/css/*.css','!app/css/**/Thumbs.db'])
+    .pipe(cssmin({
+        exclude: ['tasks'],
+        ignoreFiles: ['.map',
+                      '.scss',
+                      '.map.scss',
+                      '.min']
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+    
+});
